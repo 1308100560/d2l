c2a48b8794eef67f0237fdd6663adceadc5b6b2b
1308100560 <xinyu20010525@gmail.com>
2024-05-14 18:03:07 +0800

    更新

--- d2l/3/3.6softmax回归的从零开始实现.py
+++ d2l/3/3.6softmax回归的从零开始实现.py
@@ -25,7 +25,7 @@ X_prob = softmax(X)
 print(X_prob, X_prob.sum(1))
 
 def net(X):
-    return softmax(torch.matnul(X.reshape((-1, W.shape[0])), W) + b)
+    return softmax(torch.matmul(X.reshape((-1, W.shape[0])), W) + b)
 
 y = torch.tensor([0, 2])
 y_hat = torch.tensor([[0.1, 0.3, 0.6], [0.3, 0.2, 0.5]])
@@ -34,6 +34,53 @@ print(y_hat[0, 1], y)
 def cross_entropy(y_hat, y):
     return -torch.log(y_hat[range(len(y_hat)), y])
 
-x = cross_entropy(y_hat, y)
+print(cross_entropy(y_hat, y))
+
+def  accuracy(y_hat, y):
+    """计算预测正确的数量"""
+    if len(y_hat.shape) > 1 and y_hat.shape[1] > 1:
+        y_hat = y_hat.argmax(axis=1)
+    cmp = y_hat.type(y.dtype) == y
+    return float(cmp.type(y.dtype).sum())
+
+print(accuracy(y_hat, y) / len(y))
+
+def evaluate_accuracy(net, data_iter): #@save
+    """计算在指定数据集上模型的精度"""
+    if isinstance(net, torch.nn.Module):
+        net.eval() # 将模型设置为评估模式
+    metric = Accumulator(2) # 正确预测数、预测总数
+    with torch.no_grad():
+        for X, y in data_iter:
+            metric.add(accuracy(net(X), y), y.numel())
+    return metric[0] / metric[1]
+
+class Accumulator: #@save
+    """在n个变量上累加"""
+    def __init__(self, n):
+        self.data = [0, 0] * n
+
+    def add(self, *args):
+        self.data = [a + float(b) for a, b in zip(self.data, args)]
+
+    def reset(self):
+        self.data = [0, 0] * len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx]
+
+print(evaluate_accuracy(net, test_iter))
+
+def train_epoch_ch3(net, train_iter, loss, updater):
+    """训练模型一个迭代周期（定义见第3章）"""
+    # 将模型设置为训练模式
+    if isinstance(net, torch.nn.Module):
+        net.train()
+    # 训练损失总和、训练准确度总和、样本数
+    metric = Accumulator(3)
+    for X, y in train_iter:
+        # 计算梯度并更新参数
+        y_hat = net(X)
+
 
 breakpoint()
